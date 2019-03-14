fde1988a2740e69833505a545fa0ae6429df748a
Julia Kawano <19jkk2@panic10.cs.williams.edu>
2019-03-14 14:24:58 -0400

    Opens fat_disk file

--- fat.c
+++ fat.c
@@ -21,7 +21,24 @@
 #include <sys/xattr.h>
 #endif
 
+char cwd[256];
+char FAT[4880];
+
 static void* fat_init(struct fuse_conn_info *conn) {
+  strcat(cwd, "/fat_disk");
+  FILE *disk;
+
+  // file exists  
+  if (access(cwd, F_OK) != -1){
+    disk = fopen(cwd, "r+");
+    fclose(disk);
+  }
+  
+  // file doesn't exist
+  else {
+    disk = fopen(cwd, "w+");
+    fclose(disk);
+  }
 	return NULL;
 }
 
@@ -64,5 +81,9 @@ static struct fuse_operations fat_operations = {
 
 int main(int argc, char *argv[]) {
 	umask(0);
+	if (getcwd(cwd, sizeof(cwd))==NULL){
+	    perror("error getting current directory");
+	  }
+	// printf("%s\n", cwd);
 	return fuse_main(argc, argv, &fat_operations, NULL);
 }
